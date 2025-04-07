708eb1ff058d010b99b7dc841731dd2a9a342d34
mscreationweb <mscreation361@gmail.com>
2025-04-07 09:27:03 +0530

    bansename add

--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -36,7 +36,7 @@ const AllRoute = () => {
 
   return (
     <div className="App">
-      <BrowserRouter >
+      <BrowserRouter basename='/'>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="home" element={<Homepage />} />
